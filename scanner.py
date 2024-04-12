1b26a8b7cb6136bb24f5b09ec97489946c9feae4
irmoah80 <irmoah80@gmail.com>
2024-04-12 18:58:41 +0330

    solve wrong token saving

--- scanner.py
+++ scanner.py
@@ -44,20 +44,20 @@ def error_handler(lineerror : str , line : int):
     '''
     print errors of each line on ERROR_TABLE
     '''
-    ERROR_TABLE.append(line + '.	' + lineerror)
+    ERROR_TABLE.append(str(line) + '.	' + lineerror)
 
 def symbol_handler(linesymbols : str , line : int):
     '''
     print symbols of each line on SYMBOL_TABLE
     '''
-    SYMBOL_TABLE.append(line + '.	' + linesymbols)
+    SYMBOL_TABLE.append(str(line) + '.	' + linesymbols)
 
 
 def token_handler(linetokens : str , line : int):
     '''
     print tokens of each line on SYMBOL_TABLE
     '''
-    TOKENS.append(line + '.	' + linetokens)
+    TOKENS.append(str(line) + '.	' + linetokens)
 
 #ignore , handling in readline states
 # def exp_check(exp : str):
@@ -71,7 +71,7 @@ def token_handler(linetokens : str , line : int):
 def exp_print():
     sym = open('symbol_table.txt' , 'a')
     tkn = open('tokens.txt' , 'a')
-    err = open('lexical_errors' , 'a')
+    err = open('lexical_errors.txt' , 'a')
 
     for i in SYMBOL_TABLE:
         sym.write("%s\n" % i)
@@ -83,7 +83,7 @@ def exp_print():
         tkn.write("%s\n" % i)
 
 
-def readline(line :str):
+def readline(line :str , lno : int):
     '''
     read each line , must run from main function
     '''
@@ -97,26 +97,21 @@ def readline(line :str):
     word_type = ''
     for i in range(len(line)): # we must modify each line token on another function
         #save pervious line information
-        if ERROR_LINE != '':
-            error_handler(ERROR_LINE , i)
-        if LINE_TOKEN != '':
-            token_handler(LINE_TOKEN)
-        if SYMBL_LINE != '':
-            symbol_handler(SYMBL_LINE , i)
         
         if In_word:
             if line[i] in SYMBOL: #not space charecters
                 In_word = False
                 #maybe we need else here for turn is number off
                 if buff in KEYWORD:
-                    LINE_TOKEN += '(KEYWORD , ' + line[i] + ') '
+                    LINE_TOKEN += '(KEYWORD , ' + buff + ') '
                     #for finding () or {} errors , we need some if here , and change keyword from set to dict
                     LINE_TOKEN += '(SYMBOL , ' + line[i] + ') '
 
                 elif buff.isalpha():
                     # status = exp_check(buff)
                     # if status is -1:
-                    LINE_TOKEN += '(ID , ' + line[i] + ') '
+                    LINE_TOKEN += '(ID , ' + buff + ') '
+                    LINE_TOKEN += '(SYMBOL , ' + line[i] + ') '
                 buff=''#clear buff for next one
 
             elif line[i].isalnum():
@@ -124,8 +119,8 @@ def readline(line :str):
                 In_word = True
             elif line[i] in WH_SPACE:
                 #check buff -> what is it?
-                if buff in KEYWORD:
-                    LINE_TOKEN += '(KEYWORD , ' + line[i] + ') '
+                if buff in KEYWORD:#do not print white spaces in tokens
+                    LINE_TOKEN += '(KEYWORD , ' + buff + ') '
                 else:
                     # status = exp_check()
                     # if status is -1:
@@ -139,15 +134,22 @@ def readline(line :str):
                 buff += line[i]
                 ERROR_LINE += '(' + buff + ', Invalid number)'
         else: #just for start case , when we do not know what happened at first
-            if line[i].isnumeric:
+            if line[i].isnumeric():
                 In_num = True
-            elif line[i].isalpha:
+            elif line[i].isalpha():
                 In_word = True
             elif line[i] in WH_SPACE or SYMBOL:
                 pass
             else:
                 buff += line[i]
                 ERROR_LINE += '(' + buff + ', Invalid input)'
+
+    if ERROR_LINE != '':
+        error_handler(ERROR_LINE , i)
+    if LINE_TOKEN != '':
+        token_handler(LINE_TOKEN , i)
+    if SYMBL_LINE != '':
+        symbol_handler(SYMBL_LINE , i)
 
 
 def cleaner(clean_code : list):
@@ -165,12 +167,12 @@ def cleaner(clean_code : list):
         start_c = clean_code[i].find('/*') if not In_pro else start_c
         end_c = clean_code[i].rfind('*/')
 
-        if start_c is not -1 and end_c is not -1 :
+        if start_c != -1 and end_c != -1 :
             clean_code[i][start_c:end_c+2] = '' #remove single line comment
             In_pro= False
 
         if In_pro:
-            if end_c is not -1:
+            if end_c != -1:
                 clean_code[i][:end_c+2] = '' #remove Multiline comments: last line
             else:
                 #remove Multiline comments: mid lines
@@ -179,7 +181,7 @@ def cleaner(clean_code : list):
                     error_handler('Unclosed comment')
 
         if not In_pro: #handeling errors
-            if end_c is not -1:
+            if end_c != -1:
                 error_handler('Unmatched comment')
 
 
@@ -197,7 +199,7 @@ def main() : #done in 4 step
 
     #3.read line per line and detect sym , tkn etc.
     for i in  range(len(input)):
-        readline(input[i])
+        readline(input[i] , i)
     
     #4.print items to file
     exp_print()
